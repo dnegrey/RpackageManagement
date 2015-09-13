5b67bf2477c3a5c928ca2f8d7bef7b684601e721
dnegrey <dnegrey@gmail.com>
2015-09-13 00:34:18 -0400

    added old files

---
+++ updatePackages.R
@@ -0,0 +1,4 @@
+update.packages(lib.loc = c("/usr/lib/R/site-library", 
+                            "/usr/lib/R/library"), 
+                repos = "http://cran.case.edu", 
+                ask = FALSE)
